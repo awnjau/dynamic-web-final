002917a99b766e47bf9e54e2c7ab804d81ac8cac
awnjau <98540454+awnjau@users.noreply.github.com>
2023-12-15 02:50:52 -0500

    updated

--- src/app/components/UserProfileCard.js
+++ src/app/components/UserProfileCard.js
@@ -1,11 +1,18 @@
 import styles from "./components.module.css"
+
+
 const UserProfileCard = ({ user, emailinfo }) => {
     return(
         <div className={styles.userProfileCardStyle}>
             <p>name: {user.name}</p>
             <p>email: {emailinfo?.email}</p>
+            <img className={styles.mapimg} src="https://i.ibb.co/2hBsDhw/map.png"></img>
         </div>
+        
     );
 };
 
 export default UserProfileCard;
+
+  
+
